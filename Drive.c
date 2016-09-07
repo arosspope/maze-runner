bc29955fd1d97f136d0383cb24086fd86baed479
kate.e.leone <kate.e.leone@gmail.com>
2016-09-07 14:21:59 +1000

    WIP of Drive.

--- Drive.c
+++ Drive.c
@@ -10,44 +10,58 @@
 #include "Drive.h"
 #include "USART.h"
 #include "LCD.h"
-// defines---------
 
 
-
-//Variables---------
-                //define move function drive(direction,speed)
-                //opcode 137 [velocity high byte][velocity low byte][radius high byte][radius low byte]
-                //velocity -500 - 500mm/s
-                //radius -2000 - 2000mm
-                // straight = 32768 04 32767 = hex 8000 or 7FFF
-                //define move at distance drive(direction,speed,dist)
-                // while dist < x
-                //drive () <- equation
-
-/*put into main 
-* drive_straight();
-* drive_square();
+/* define move function drive(direction,speed)
+* opcode 137 [velocity high byte][velocity low byte][radius high byte][radius low byte]
+* velocity -500 - 500mm/s
+* radius -2000 - 2000mm
+* straight = 32768 04 32767 = hex 8000 or 7FFF
+* define move at distance drive(direction,speed,dist)
+* while dist < x
 */
+                
 
-void drive(int16_t vel, int16_t rad){    //vel = velocity in mm/sec 0->500, rad = radius of turn
-  int16union_t velBytes, radBytes;
+
+
+void drive(int16_t vel, int16_t rad, int16_t distance){    //vel = velocity in mm/sec 0->500, rad = radius of turn
+  int16union_t velBytes, radBytes, rxdata;
+  int16_t disttravelled = 0;
   velBytes.l = vel;
   radBytes.l = rad;
+  
+  USART_OutChar(142);
+  USART_OutChar(19);
+  USART_InChar();
+  USART_InChar();
   
   USART_OutChar(137);
   USART_OutChar(velBytes.s.Hi);   //send the high bit velocity
   USART_OutChar(velBytes.s.Lo);    //send the low bit velocity
   USART_OutChar(radBytes.s.Hi);   //send the high bit radius
   USART_OutChar(radBytes.s.Lo);    //send the low bit radius
+  
+  while(disttravelled < distance) {
+        USART_OutChar(142);
+        USART_OutChar(19);
+        rxdata.s.Hi = USART_InChar();
+        rxdata.s.Lo = USART_InChar();
+        
+        disttravelled += rxdata.l;
+  }
+  
+  USART_OutChar(137);
+  USART_OutChar(0);   //send the high bit velocity
+  USART_OutChar(0);    //send the low bit velocity
+  USART_OutChar(0);   //send the high bit radius
+  USART_OutChar(0);    //send the low bit radius
 }
 
 void driveStraight(int16_t vel, int dist){ //vel = velocity in mm/sec 0->500, dist = distance in mm
   int i;
   //@Note: At the moment, this code does not actually keep track of distance traveled.
   //       To fix this, we must get the relevant packet from the irobot that returns distance travelled
-  for(i = 0; i < dist; i++){
-    drive(vel, 32768);
-  }
+
   
-  drive(0, 32768); //This stops the motor from turning (i.e. velocity = 0)
+  drive(vel, 32768, dist); //This stops the motor from turning (i.e. velocity = 0)
 }
