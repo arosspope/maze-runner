ebda0f71a01ea76bcfa35662ffcebe5644b6ea88
kate.e.leone <kate.e.leone@gmail.com>
2016-09-28 14:34:20 +1000

    ITS A MEE, MARIO

--- IROBOT.c
+++ IROBOT.c
@@ -21,12 +21,12 @@
 /* Loading Song Notes to EEPROM (pre-loading only) - TODO: Choose notes */
 //Note duration is stored next to the note:
 //  -e.g. DATA(D, 0.5s, C#, 0.2s); 'D' will play for 0.5seconds, 'C#' will play for 0.2s
-__EEPROM_DATA(71, 32, 76, 31, 79, 31, 78, 33);  //Song 0 - ADDR offset: 0x00 Harry Potter Victim 1
+__EEPROM_DATA(71, 32, 76, 31, 79, 31, 78, 33);  //Song 0 - ADDR offset: 0x00 Harry Potter-Victim 1
 __EEPROM_DATA(76, 34, 71, 33, 69, 31, 78, 34);
-__EEPROM_DATA(98, 30, 103, 30, 110, 25, 108, 10);  //Song 1 - ADDR offset: 0x10 Star Wars Victim 2
+__EEPROM_DATA(98, 30, 103, 30, 110, 25, 108, 10);  //Song 1 - ADDR offset: 0x10 Star Wars-Victim 2
 __EEPROM_DATA(107, 10, 105, 10, 115, 25, 110, 20);
-__EEPROM_DATA(1, 2, 3, 4, 5, 6, 7, 8);          //Song 2 - ADDR offset: 0x20
-__EEPROM_DATA(9, 10, 11, 12, 13, 14, 15, 16);
+__EEPROM_DATA(100, 10, 100, 10, 100, 10, 96, 20);          //Song 2 - ADDR offset: 0x20 Mario-Finish 
+__EEPROM_DATA(100, 20, 103, 20, 91, 30, 88, 20);
 __EEPROM_DATA(1, 2, 3, 4, 5, 6, 7, 8)  ;          //Song 3 - ADDR offset: 0x30
 __EEPROM_DATA(9, 10, 11, 12, 13, 14, 15, 16);
 
